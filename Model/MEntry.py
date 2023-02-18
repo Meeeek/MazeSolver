adf029a714b8f76b4d906f4c8058b4dfa3a2dba8
Meeeek <conradfukuzawa@hotmail.ca>
2023-02-18 16:48:10 -0500

    Added min path functionality

--- Model/MEntry.py
+++ Model/MEntry.py
@@ -7,7 +7,7 @@ class MEntry:
         self.right = right
         self.up = up
         self.down = down
-        self.status = 0 # 0 means unvisited, 1 means queued, 2 means visited, 3 means inaccessible, 4 means target
+        self.status = 0 # 0 means unvisited, 1 means queued, 2 means visited, 3 means inaccessible, 4 means target, 5 is path
     
     def get_color(self):
         if self.status == 0:
@@ -16,6 +16,8 @@ class MEntry:
             return (255,191,0)
         elif self.status == 2:
             return (0,255,0)
-        elif self.status ==4:
+        elif self.status == 4:
             return (255,0,0)
+        elif self.status == 5:
+            return (0,0,255)
         return (0,0,0)
--- ViewModel/BFS.py
+++ ViewModel/BFS.py
@@ -8,20 +8,20 @@ class BFS(AlgoBase):
         super().__init__(maze, cp)
         # initialize queue
         self.q = Queue()
-        self.q.put((maze.get(cp), 0))
+        self.q.put((maze.get(cp), [cp]))
         self.total_steps = 0
         self.maze = maze
 
     def change_cp(self, cp):
-        ele, path_size = self.q.get()
+        ele, path = self.q.get()
         ele.status = 0
         self.q = Queue()
         o = self.maze.get(cp)
-        self.q.put((o, 0))
+        self.q.put((o, [cp]))
         o.status = 1
 
     def step(self) -> bool:
-        ele, path_size = self.q.get()
+        ele, path = self.q.get()
         self.total_steps += 1
         if not ele: 
             return True
@@ -29,11 +29,14 @@ class BFS(AlgoBase):
         # get and append neighbors
         for o in [ele.left, ele.right, ele.up, ele.down]:
             if o and o.status == 0:
-                self.q.put((o, path_size + 1))
+                self.q.put((o, path + [o.cp]))
                 o.status = 1
             elif o and o.status == 4:
                 print(f'Total steps: {self.total_steps}')
-                print(f'Minimum path: {path_size}')
+                print(f'Minimum path: {len(path)}')
+                print(f'Path: {path}')
+                for p in path:
+                    self.maze.get(p).status = 5
                 return True # found target
         
         ele.status = 2
